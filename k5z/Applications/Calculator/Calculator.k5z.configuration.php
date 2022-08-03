f8af5de1cda5443de45688b78bb3f0e6762c2bea
Krists Krigers <krists.krigers@gmail.com>
2022-08-04 00:19:38 +0300

    Add simpler calculator example for Muffin demo

---
+++ k5z/Applications/Calculator/Calculator.k5z.configuration.php
@@ -0,0 +1,26 @@
+<?php
+// Copyright (c) 2021 Krists Krigers <krists dot krigers at gmail dot com>. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+return [
+    'javascript_links' => [
+        'jquery.js' => 'https://code.jquery.com/jquery-3.2.1.min.js',
+        'bootstrap.js' => 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+        'popper.js' => 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
+    ],
+    'css_links' => [
+        'bootstrap.css' => 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+    ],
+    'core.active' => TRUE,
+    'core.autostart' => TRUE,
+//    'core.debug.enabled' => TRUE,
+    'core.stats.enabled' => TRUE,
+    'core.debug.filename' => TRUE,
+    'core.time_limit' => 1,
+    'core.recompile.enabled' => TRUE,
+    'core.recompile.command' => '../../../this-k5z',
+//    'xhprof.enabled' => TRUE,
+//    'xhprof.path' => '/Users/kristsk/Projects/GEN5/php5-xhprof',
+    'web_application.wipe_on_recompile' => TRUE,
+    'web_application.dev_tools.enabled' => TRUE
+];
